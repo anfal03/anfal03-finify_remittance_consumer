c44a378b76e1359a042b30028c288705dc4a0b0f
zintesar <zintesar@gmail.com>
2023-07-20 18:15:25 +0600

    set new pause time

--- src/modules/agent/agent.controller.ts
+++ src/modules/agent/agent.controller.ts
@@ -110,7 +110,7 @@ export class AgentController {
   }
 
   // @Cron(CronExpression.EVERY_30_SECONDS)
-    @Cron('0 10 18  * * *')
+    @Cron('0 30 12  * * *')
     // @Cron('0 1/2 * * * *')
     // @Post("/kafakjs-pause")
     async pauseConsumer(@Payload() data: any): Promise<void> {
@@ -122,7 +122,7 @@ export class AgentController {
     }
 
     // @Cron(CronExpression.EVERY_MINUTE)
-    @Cron('0 25 18 * * * *')
+    @Cron('0 40 12 * * * *')
     // @Cron('0 */2 * * * *')
     // @Post("/kafakjs-resume")
     async resumeConsumer(@Payload() data: any): Promise<void> {
